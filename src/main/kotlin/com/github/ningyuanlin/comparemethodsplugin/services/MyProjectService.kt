3e9367f69f1a29eefab438ffc15b79ab15c799fd
GitHub Action <action@github.com>
2025-03-19 02:51:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/ningyuanlin/comparemethodsplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ningyuanlin/comparemethodsplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.ningyuanlin.comparemethodsplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ningyuanlin.comparemethodsplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
